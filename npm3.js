d16e2658713cd915a02e59e52ab29253dcb1f4e7
dhinagarankalai <152267168+dhinagarankalai@users.noreply.github.com>
2023-11-28 15:32:36 +0530

    npm package3

---
+++ npm3.js
@@ -0,0 +1,13 @@
+const { request } = require('http');
+
+const fastify = require('fastify')({
+    logger:true
+});
+fastify.get('/', async (request, reply) => {
+    reply.type('application/json').code(200);
+    return { hello: 'world'};
+});
+fastify.listen(30000, (err, address) => {
+    if (err) throw err;
+    fastify.log.info('App listening on ${adress}');
+});
